1d9ab600162a9d7e8f5f9bb0da7d42d6cbebe13e
mmjafari <mmjafari@openmailbox.org>
2019-05-22 07:58:48 +0900

    Added Markov Function for Next N steps

--- Iodin.sh
+++ Iodin.sh
@@ -5,7 +5,7 @@ NuMsinCls () {
 NuMsinSub () {
 	for i in $(echo $1 | tr ' ' "\n" | grep '('); do
 		i=$(echo -ne $i | sed 's/(//g')
-		echo -ne "$(cat $fNam.stscs | sed -n '/-零-/,/-壱-/p' | grep $i --count)∃$i "
+		echo -ne "$(cat $fNam.stscs | sed -n '/-零-/,/-弌-/p' | grep $i --count)∃$i "
 	done | sed 's/ /\n/g' | sort --general-numeric-sort --reverse |sed 's/\n/ /g'
 }
 arra=$(cat $1 | grep '\[' | sed 's/\[//g')
@@ -13,7 +13,7 @@ arra=$(cat $1 | grep '\[' | sed 's/\[//g')
 fNam=$1
 echo "--------零--------" > $1.stscs
 for i in $(cat $1 | sort --general-numeric-sort | grep -v "(\|)\|\[\|\]" | sed 's/\t//g'); do $(cat ~/.SepiDiv)/Argon/Argon.sh -f $1 -w $i; done >> $1.stscs
-echo "--------壱--------" >> $1.stscs
+echo "--------弌--------" >> $1.stscs
 for i in $arra; do
 	cat $1 | sed -n "/^\[$i/,/^\]$/p" > cls$i;
 	echo -ne "$(NuMsinCls $i)Ɐ$i ";
@@ -23,15 +23,15 @@ for i in $arra; do
 	echo -ne "\n"
 done | sort --general-numeric-sort --reverse | sed '/^$/d' >> $1.stscs
 echo "--------弐--------" >> $1.stscs
-chek1=$(cat $fNam.stscs | sed -n '/-零-/,/-壱-/p' | sed '/^--------/d' | sed 's/.*=>//g' | sort | uniq --count | wc -l)
-chek2=$(cat $fNam.stscs | sed -n '/-零-/,/-壱-/p' | sed '/^--------/d' | wc -l)
+chek1=$(cat $fNam.stscs | sed -n '/-零-/,/-弌-/p' | sed '/^--------/d' | sed 's/.*=>//g' | sort | uniq --count | wc -l)
+chek2=$(cat $fNam.stscs | sed -n '/-零-/,/-弌-/p' | sed '/^--------/d' | wc -l)
 if [ $chek1 = $chek2 ]; then
 	echo "開"
 else
 	echo "閉"
 fi >> $1.stscs
 echo "--------酸--------" >> $1.stscs
-cat $fNam.stscs | sed -n '/-零-/,/-壱-/p' | sed '/^--------/d' | awk -F'=>' '{NF-=1}1' | sed 's/ /=>/g' > clstmpr
+cat $fNam.stscs | sed -n '/-零-/,/-弌-/p' | sed '/^--------/d' | awk -F'=>' '{NF-=1}1' | sed 's/ /=>/g' > clstmpr
 touch clsmrk
 for line in $(cat clstmpr); do
 	if [ $(cat clsmrk | grep $line | wc -l) = 0 ]; then
@@ -52,7 +52,7 @@ for i in $(seq 1 $(($(cat clsMarkov | wc -l)))); do
 	echo $iが$line
 done >> $1.stscs
 echo '--------死--------' >> $1.stscs
-echo "--------参--------" >> $1.stscs
+echo "--------弎--------" >> $1.stscs
 res=0
 for line in $(cat $fNam.stscs | sed -n '/-酸-/,/-死-/p' | sed '/^--------/d' | awk -F'が' '{print $2}');do
 	res=$(($res + $line))
@@ -64,4 +64,21 @@ for line in $(cat $fNam.stscs | sed -n '/-酸-/,/-死-/p' | sed '/^--------/d' |
 done | sort -t'%' -k2 --general-numeric-sort --reverse | sed 's/.00//g'>> $1.stscs
 sed -i '/-酸-/,/-死-/d' $1.stscs
 echo '--------肆--------' >> $1.stscs
+zaLasto () {
+	zaLina=$(cat clstmpr | tail -n 1)
+	laSum=0
+	for line in $(cat $fNam.stscs | sed -n '/-弎-/,/-肆-/p' | sed '/^--------/d' | grep --regexp "^\[$zaLina"); do
+		zaCount=$(echo $line | awk -F'%' '{print $2}')
+		yes "$(echo $line | awk -F':' '{print $1}')" | head -n $zaCount >> clsNext
+	done
+	rand=$(echo $(($(date +%s%N) % $(cat clsNext | wc -l) + 1)))
+	cat clsNext | sed "${rand}q;d" | awk -F',' '{print $2}' | sed 's/\]//g' >> clstmpr
+}
+if [ ! $2 = '' ]; then
+	for i in $(seq 1 $2); do
+		zaLasto
+	done
+fi
+cat clstmpr
+echo '--------伍--------' >> $1.stscs
 rm cls*
